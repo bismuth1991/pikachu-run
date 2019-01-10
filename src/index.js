c6b3eb495669a92fe1592931ceb812db27954d99
bismuth1991 <pbao.tran.unsw@gmail.com>
2019-01-10 09:38:45 -0800

    Add css normalize 8.0.1 css reset

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import GameCanvas from './game/game_canvas';
 
+import './assets/css/normalize_8_0_1.css';
+
 window.addEventListener('DOMContentLoaded', () => {
   const canvas = document.querySelector('canvas');
   const ctx = canvas.getContext('2d');
