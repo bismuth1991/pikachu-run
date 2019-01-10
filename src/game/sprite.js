3898f903e16de6575707904102e4b289cbb7f365
bismuth1991 <pbao.tran.unsw@gmail.com>
2019-01-10 09:12:15 -0800

    Create Sprite class

---
+++ src/game/sprite.js
@@ -0,0 +1,26 @@
+class Sprite {
+  constructor(image, sheetHeight, sheetWidth, rows, frameCount) {
+    this.image = image;
+    this.height = sheetHeight / rows;
+    this.width = sheetWidth / frameCount;
+    this.frameCount = frameCount;
+    this.currentFrame = 0;
+    this.isMoveLeft = false;
+    this.isMove = false;
+    this.isTeleport = false;
+  }
+
+  srcX() {
+    return this.currentFrame * this.width;
+  }
+
+  srcY() {
+    return this.isMoveLeft ? this.height : 0;
+  }
+
+  updateFrame() {
+    this.currentFrame = (this.currentFrame + 1) % this.frameCount;
+  }
+}
+
+export default Sprite;
