5320584cc8da364b89636c11f6f05fd41c0108ea
bismuth1991 <pbao.tran.unsw@gmail.com>
2019-01-11 22:50:06 -0500

    Move pikachu sprite sheets into different file

---
+++ src/game/pikachu/pikachu_sprite_sheets.js
@@ -0,0 +1,23 @@
+import Sprite from '../sprite';
+
+const pikachuStaticSpriteSheet = require('../../assets/images/pikachu-standing.png');
+const pikachuRunRightSpriteSheet = require('../../assets/images/pikachu-running-right.png');
+const pikachuRunLeftSpriteSheet = require('../../assets/images/pikachu-running-left.png');
+const pikachuRollSpriteSheet = require('../../assets/images/pikachu-rolling.png');
+
+const pikachuStatic = new Image();
+pikachuStatic.src = pikachuStaticSpriteSheet;
+
+const pikachuRunRight = new Image();
+pikachuRunRight.src = pikachuRunRightSpriteSheet;
+
+const pikachuRunLeft = new Image();
+pikachuRunLeft.src = pikachuRunLeftSpriteSheet;
+
+const pikachuRoll = new Image();
+pikachuRoll.src = pikachuRollSpriteSheet;
+
+export const pikachuStaticSprite = new Sprite(pikachuStatic, 96, 161, 2, 4);
+export const pikachuRunRightSprite = new Sprite(pikachuRunRight, 48, 240, 1, 4);
+export const pikachuRunLeftSprite = new Sprite(pikachuRunLeft, 48, 240, 1, 4);
+export const pikachuRollSprite = new Sprite(pikachuRoll, 96, 192, 2, 4);
