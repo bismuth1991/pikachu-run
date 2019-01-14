8323f357668b96a75c2f5956e36684fd003481aa
bismuth1991 <pbao.tran.unsw@gmail.com>
2019-01-13 19:19:25 -0500

    Add meteors to initialAssets

--- src/game/assets.js
+++ src/game/assets.js
@@ -1,6 +1,7 @@
 import Pikachu from './pikachu/pikachu';
 import Bubble from './bubbles/bubble';
 import Spearow from './obstacles/spearow';
+import Meteor from './draco_meteors/meteor';
 
 const pikachu = new Pikachu();
 
@@ -11,6 +12,7 @@ const obstacles = [
 const initialAssets = {
   pikachu,
   bubbles: Bubble.makeBubbles(),
+  meteors: Meteor.makeMeteor(),
   obstacles,
 };
 
