81f29c57a9817f5e074141659f2052fa583bdcd1
bismuth1991 <pbao.tran.unsw@gmail.com>
2019-01-14 01:23:43 -0500

    .

--- src/game/game_canvas/draw_meteor.js
+++ src/game/game_canvas/draw_meteor.js
@@ -1,22 +1,22 @@
-const drawMiniMeteor = (meteor, ctx) => {
+const drawMiniMeteor = (miniMeteors, ctx) => {
   ctx.save();
-  ctx.translate(meteor.physics.x, meteor.physics.y);
+  ctx.translate(miniMeteors.physics.x, miniMeteors.physics.y);
 
   ctx.beginPath();
-  ctx.arc(0, 0, meteor.radius, 0, Math.PI * 2, false);
+  ctx.arc(0, 0, miniMeteors.radius, 0, Math.PI * 2, false);
 
-  ctx.shadowColor = meteor.color;
+  ctx.shadowColor = miniMeteors.color;
   ctx.shadowBlur = 20;
   ctx.shadowOffsetX = 0;
   ctx.shadowOffsetY = 0;
 
-  ctx.fillStyle = meteor.color;
+  ctx.fillStyle = miniMeteors.color;
   ctx.fill();
   ctx.closePath();
 
   ctx.restore();
 
-  meteor.physics.updatePos();
+  miniMeteors.physics.updatePos();
 };
 
 const drawMeteor = (meteor, ctx) => {
