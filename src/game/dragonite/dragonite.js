5d4af6c98c93b9306424c83169abc34a23c8c4da
bismuth1991 <pbao.tran.unsw@gmail.com>
2019-01-14 11:22:59 -0800

    Add invert background effext when Dragonite uses DracoMeteor

--- src/game/dragonite/dragonite.js
+++ src/game/dragonite/dragonite.js
@@ -3,11 +3,11 @@ import dragonitePhysics from './dragonite_random_generators';
 import { NUM_DRAGONITE, DRAGONITE_METEORS_DEACTIVATION_X } from '../constant';
 import Meteor from './meteor';
 
-const dragoniteSpriteSheet = require('../../assets/images/dragonite.png');
+const dragoniteSpriteSheet = require('../../assets/images/dragonite-small.png');
 
 const dragonite = new Image();
 dragonite.src = dragoniteSpriteSheet;
-const dragoniteSprite = new Sprite(dragonite, 376, 2640, 2, 11);
+const dragoniteSprite = new Sprite(dragonite, 252, 1760, 2, 11);
 
 class Dragonite {
   constructor() {
@@ -27,6 +27,10 @@ class Dragonite {
     return dragonites;
   }
 
+  static getCanvasBackground() {
+    return document.getElementsByClassName('canvas-container')[0];
+  }
+
   isOutOfBounds() {
     return this.physics.x + this.sprite.width < DRAGONITE_METEORS_DEACTIVATION_X;
   }
@@ -38,11 +42,17 @@ class Dragonite {
   activateDracoMeteor() {
     this.meteors = Meteor.makeMeteor();
     this.isMeteorsActivate = true;
+
+    const canvasBackground = Dragonite.getCanvasBackground();
+    canvasBackground.classList.add('filter-invert');
   }
 
   deactivateDracoMeteor() {
     this.meteors = [];
     this.isMeteorsActivate = false;
+
+    const canvasBackground = Dragonite.getCanvasBackground();
+    canvasBackground.classList.remove('filter-invert');
   }
 }
 
