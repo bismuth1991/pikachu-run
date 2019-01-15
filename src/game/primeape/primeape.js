07c4e60dcda511f0d834bbbb5803f46db03f8320
bismuth1991 <pbao.tran.unsw@gmail.com>
2019-01-14 16:39:58 -0800

    Add run, stop, punch, isOutOfBounds, and resetPhysics methods for Primeape

--- src/game/primeape/primeape.js
+++ src/game/primeape/primeape.js
@@ -1,19 +1,37 @@
+import {
+  primeapeStaticSprite,
+  primeapeRunSprite,
+  primeapePunchSprite,
+} from './primeape_sprite_sheets';
 
 class Primeape {
   constructor() {
     this.physics = primeapePhysics();
-    this.sprite =
+    this.sprite = primeapeStaticSprite;
   }
 
   run() {
-
+    this.sprite = primeapeRunSprite;
+    this.dLeft = PRIMEAPE_SPEED;
   }
 
   stop() {
-
+    this.sprite = primeapeStaticSprite;
+    this.dLeft = 0;
   }
 
   punch() {
-    
+    this.sprite = primeapePunchSprite;
+    this.dLeft = 0;
+  }
+
+  isOutOfBounds() {
+    return this.physics + this.sprite.width < 0;
+  }
+
+  resetPhysics() {
+    this.physics = primeapePhysics();
   }
 }
+
+export default Primeape;
