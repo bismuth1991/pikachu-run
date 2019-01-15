a1c8307eaa34f1d28a7903f63f61e3d155748779
bismuth1991 <pbao.tran.unsw@gmail.com>
2019-01-15 04:15:18 -0500

    Add pikachuTakeDamageSprite

--- src/game/pikachu/pikachu_sprite_sheets.js
+++ src/game/pikachu/pikachu_sprite_sheets.js
@@ -5,6 +5,7 @@ const pikachuRunRightSpriteSheet = require('../../assets/images/pikachu-running-
 const pikachuRunLeftSpriteSheet = require('../../assets/images/pikachu-running-left.png');
 const pikachuRunSpriteSheet = require('../../assets/images/pikachu-running.png');
 const pikachuRollSpriteSheet = require('../../assets/images/pikachu-rolling.png');
+const pikachuTakeDamageSpriteSheet = require('../../assets/images/pikachu-take-damage.png');
 
 const pikachuStatic = new Image();
 pikachuStatic.src = pikachuStaticSpriteSheet;
@@ -22,8 +23,12 @@ pikachuRun.src = pikachuRunSpriteSheet;
 const pikachuRoll = new Image();
 pikachuRoll.src = pikachuRollSpriteSheet;
 
+const pikachuTakeDamage = new Image();
+pikachuTakeDamage.src = pikachuTakeDamageSpriteSheet;
+
 export const pikachuStaticSprite = new Sprite(pikachuStatic, 96, 161, 2, 4);
 export const pikachuRunRightSprite = new Sprite(pikachuRunRight, 48, 240, 1, 4);
 export const pikachuRunLeftSprite = new Sprite(pikachuRunLeft, 48, 240, 1, 4);
 export const pikachuRunSprite = new Sprite(pikachuRun, 96, 240, 2, 4);
 export const pikachuRollSprite = new Sprite(pikachuRoll, 96, 192, 2, 4);
+export const pikachuTakeDamageSprite = new Sprite(pikachuTakeDamage, 96, 294, 2, 6);
