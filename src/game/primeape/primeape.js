b77e8c6dbfe9b85a74f1fd24268ee3297432ea90
bismuth1991 <pbao.tran.unsw@gmail.com>
2019-01-14 16:35:03 -0800

    Create Primeape class skeleton and sprites)

---
+++ src/game/primeape/primeape.js
@@ -0,0 +1,19 @@
+
+class Primeape {
+  constructor() {
+    this.physics = primeapePhysics();
+    this.sprite =
+  }
+
+  run() {
+
+  }
+
+  stop() {
+
+  }
+
+  punch() {
+    
+  }
+}
---
+++ src/game/primeape/primeape_sprite_sheets.js
@@ -0,0 +1,18 @@
+import Sprite from '../sprite';
+
+const primeapeStaticSpriteSheet = require('../../assets/images/primeape-stand.png');
+const primeapeRunSpriteSheet = require('../../assets/images/primeape-run.png');
+const primeapePunchSpriteSheet = require('../../assets/images/primeape-punch.png');
+
+const primeapeStatic = new Image();
+primeapeStatic.src = primeapeStaticSpriteSheet;
+
+const primeapeRun = new Image();
+primeapeRun.src = primeapeRunSpriteSheet;
+
+const primeapePunch = new Image();
+primeapePunch.src = primeapePunchSpriteSheet;
+
+export const primeapeStaticSprite = new Sprite(primeapeStatic, 69, 748, 1, 11);
+export const primeapeRunSprite = new Sprite(primeapeRun, 69, 740, 1, 10);
+export const primeapePunchSprite = new Sprite(primeapePunch, 69, 1740, 1, 12);
